93a306f86e14ee370756de98582bb8de577ebd52
CodePy3 <jbmonks666@icloud.com>
2026-02-09 10:07:54 +0000

    Add student information and query description

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -1,3 +1,6 @@
+# Name: Jonathan Monks
+# StudentID: 201968903
+
 -- For each student, calculate the total number of credits from courses they passed. Assume a passing grade is 40 or higher.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, TotalCreditsPassed
